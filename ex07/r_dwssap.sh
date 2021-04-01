b2a50bb4a89f427f6caa1c5d76df14c371ca35b2
vcavalca <vcavalca@student.42sp.org.br>
2021-04-01 00:20:32 +0000

    Atualizado o exercicio

--- ex07/r_dwssap.sh
+++ ex07/r_dwssap.sh
@@ -1,3 +1,4 @@
 #!bin/sh
-
-cat /etc/passwd | grep \# -v | sed -n g\;n\;p | cut -d ':' -f 1 | rev | sort -r | tr '\n' ',' | sed 's/,/, /g;s/, $/./' | tr -d '\n'
+FT_LINE1=7
+FT_LINE2=15
+cat /etc/passwd | awk -F ":" '{print $1}' |  awk '0==NR%2' | rev | sort -r | sed -n "${FT_LINE1},${FT_LINE2} p" | paste -s -d "," | tr "\n" "."
