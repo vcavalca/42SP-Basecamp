3b924c5ae47caa7b641de8575f78e453cb82d935
vcavalca <vcavalca@student.42sp.org.br>
2021-04-04 15:48:26 +0000

    Corrigido com norminnete -RChek..

--- ex00/main.c
+++ ex00/main.c
@@ -6,16 +6,14 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/02 23:23:30 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/04/04 15:24:39 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/04/04 15:47:39 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	rush(int x, int y);
 
-int		main()
+int		main(void)
 {
 	rush(4, 4);
 	return (0);
-
-	ft_putchar(c);
 }
