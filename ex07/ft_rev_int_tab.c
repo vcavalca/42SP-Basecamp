52f92642528c71fe0e65443ee6601be3dd83ff6e
vcavalca <vcavalca@student.42sp.org.br>
2021-04-08 16:37:17 +0000

    Atualizado o exercicio

--- ex07/ft_rev_int_tab.c
+++ ex07/ft_rev_int_tab.c
@@ -6,45 +6,28 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 16:45:28 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/04/08 13:46:43 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/04/08 16:25:47 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
 void	ft_rev_int_tab(int *tab, int size)
 {
-	int	i;
-	int	trade;
+	int index;
+	int i;
+	int aux[size];
 
-	size--;
 	i = 0;
-	while (size >= i)
+	index = size - 1;
+	while (index >= 0)
 	{
-		trade = tab[size];
-		tab[size] = tab[i];
-		tab[i] = trade;
-		size--;
+		aux[i] = tab[index];
+		index--;
 		i++;
 	}
-}
-
-int        main(void)
-{
-    int array[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int size;
-
-    size = 11;
-
-    ft_rev_int_tab(array, size);
-
-    int count = 0;
-
-    while (count < size)
-    {
-        printf(" %d ", array[count]);
-        count++;
-    }
-
-    return (0);
+	i = 0;
+	while (i < size)
+	{
+		tab[i] = aux[i];
+		i++;
+	}
 }
