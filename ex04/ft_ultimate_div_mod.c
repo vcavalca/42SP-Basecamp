aaf9bf00bc3eefad83b91487b2e992ef0910a551
vcavalca <vcavalca@student.42sp.org.br>
2021-04-08 16:40:39 +0000

    Norma

--- ex04/ft_ultimate_div_mod.c
+++ ex04/ft_ultimate_div_mod.c
@@ -6,7 +6,7 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 11:55:49 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/04/08 16:29:13 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/04/08 16:39:52 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_ultimate_div_mod(int *a, int *b)
 	int auxb;
 
 	auxa = *a;
-	auxb =*b;
+	auxb = *b;
 	*a = auxa / auxb;
 	*b = auxa % auxb;
 }
