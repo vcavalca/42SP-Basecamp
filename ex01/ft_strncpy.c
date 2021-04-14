25cd49b1c863d0a91a7b5a2ef1fb95337b563a0e
vcavalca <vcavalca@student.42sp.org.br>
2021-04-14 16:03:45 +0000

    norma

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,24 +6,24 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/08 19:11:57 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/04/14 15:53:45 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/04/14 16:01:51 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char        *ft_strncpy(char *dest, char *src, unsigned    int n)
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    i = 0;
-    while (i < n && src[i] != '\0')
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    while (i < n)
-    {
-        dest[i] = '\0';
-        i++;
-    }
-    return (dest);
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
 }
--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -6,32 +6,32 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/13 19:31:24 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/04/14 15:54:01 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/04/14 16:01:21 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned int    ft_strlcpy(char *dest, char *src, unsigned int size)
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
-    unsigned int count;
-    unsigned int position;
-    unsigned int hold;
+	unsigned int count;
+	unsigned int position;
+	unsigned int hold;
 
-    hold = 0;
-    count = 1;
-    position = 0;
-    while (src[hold])
-    {
-        hold++;
-    }
-    if (hold != 0)
-    {
-        while (count < size)
-        {
-            dest[position] = src[position];
-            count++;
-            position++;
-        }
-    }
-    dest[position] = '\0';
-    return (hold);
+	hold = 0;
+	count = 1;
+	position = 0;
+	while (src[hold])
+	{
+		hold++;
+	}
+	if (hold != 0)
+	{
+		while (count < size)
+		{
+			dest[position] = src[position];
+			count++;
+			position++;
+		}
+	}
+	dest[position] = '\0';
+	return (hold);
 }
